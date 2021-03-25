17b3d2fe9e3cd95d359112816471480e2fb966de
Roshini <roshbhaskar25@gmail.com>
2021-03-25 17:50:20 +0530

    skeleton

--- pathvisualizer/src/Pathfinder/node/node.js
+++ pathvisualizer/src/Pathfinder/node/node.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
-
+import './node.css';
 export default class Node extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
     render() {
         return (
-            <div>
+            <div className="Node">
                 HI from node
             </div>
         )
--- pathvisualizer/src/Pathfinder/pathfinder.js
+++ pathvisualizer/src/Pathfinder/pathfinder.js
@@ -1,7 +1,15 @@
-import React, { Component } from 'react'
-import Node from './node/node.js'
-
+import React, { Component } from 'react';
+import Node from './node/node.js';
+import './pathfinder.css';
 export default class Pathfinder extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
     render() {
         return (
             <div>
